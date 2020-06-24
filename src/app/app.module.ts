538f727224943f64549160c5e7f741652fb764fc
Bartek <WonderfulLifeVision@gmail.com>
2020-06-24 12:10:22 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CaptionComponent } from './caption/caption.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaptionComponent
   ],
   imports: [
     BrowserModule
